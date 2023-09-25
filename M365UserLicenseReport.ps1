0cec260974394a255446664ce2b05c6618070974
Manuel Nieto <manuel.nieto@outlook.com>
2023-09-25 02:39:22 -0500

    Draft V0.1

---
+++ M365UserLicenseReport.ps1
@@ -0,0 +1,224 @@
+﻿<#
+.SYNOPSIS
+Script to get detailed insights into user license assignments within an Azure AD environment. It extracts information about assigned licenses, services, and their statuses for each user.
+
+.DESCRIPTION
+The script performs the following key functions:
+    1. **User License Information Retrieval:**
+    - Extracts and consolidates information about assigned licenses, services, and their statuses for each user.
+    - Provides the option to process a specified list of users or all users within the Azure AD environment.
+    - Generates detailed and simplified reports in CSV format.
+
+    2. **Azure AD Module Validation and Installation:**
+    - Validates the presence of the AzureAD module and facilitates its installation if absent.
+
+    3. **Local Administrator Privilege Validation:**
+    - Verifies the script is executed with local administrator privileges.
+
+    4. **Friendly Name Mapping:**
+    - Converts SKU IDs and Service Names to more readable friendly names.
+    - Utilizes predefined friendly name lists for license plans and services.
+
+    5. **Progress Display:**
+    - Provides progress update during execution, indicating user being processed and total number of users exported.
+
+.PARAMETER UserNamesFile
+An optional parameter allowing the user to specify a CSV file containing a list of users to be processed. If not provided, the script will process all users in the Azure AD environment.
+
+.EXAMPLE
+.\ScriptName.ps1 -UserNamesFile .\UserList.csv
+
+Example demonstrates how to run script with a specified list of users contained in the 'UserList.csv' file.
+
+.OUTPUTS
+1. **Detailed Report (CSV):**
+   - Contains comprehensive details such as DisplayName, UserPrincipalName, LicensePlan, FriendlyNameofLicensePlan, ServiceId, ServiceName, and ProvisioningStatus.
+
+2. **Simplified Report (CSV):**
+   - Provides a concise view containing DisplayName, UserPrincipalName, Country, LicensePlanWithEnabledService, and FriendlyNameOfLicensePlanAndEnabledService.
+
+.NOTES
+- Ensure the script is run with local administrator privileges.
+- Allow the script to install the AzureAD module when prompted if it is not already installed.
+- Uncomment the last section of the script to receive a prompt to open the output files immediately after the script execution.
+
+#>
+
+Param
+(
+ [Parameter(Mandatory = $false)]
+    [string]$UserNamesFile
+)
+
+
+Function Validate-LocalAdmin {
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $windowsPrincipal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+    return $windowsPrincipal.IsInRole($adminRole)
+}
+
+Function Validate-AzureADModule {
+    if (-not (Get-Module -ListAvailable -Name AzureAD)) {
+        $userConfirmation = Read-Host "AzureAD module is not installed. Would you like to install it now? (Y/N)"
+        if ($userConfirmation -eq 'Y' -or $userConfirmation -eq 'y') {
+            Install-Module -Name AzureAD -Force -AllowClobber -Scope CurrentUser
+        } else {
+            Write-Host "AzureAD module is required for this script to run. Exiting..."
+            exit
+        }
+    }
+}
+
+Function Get_UsersLicenseInfo {
+  $upn = $_.UserPrincipalName
+  $Country = $_.Country
+  if ([string]$Country -eq "") {
+      $Country = "-"
+  }
+  Write-Progress -Activity "`n     Exported user count:$LicensedUserCount "`n"Currently Processing:$upn"
+  
+  $Skus = $_.AssignedLicenses
+  $LicensePlanWithEnabledService = ""
+  $FriendlyNameOfLicensePlanWithService = ""
+  
+  foreach ($Sku in $Skus) {
+      $LicenseItem = $Sku.SkuId
+      $EasyName = $global:LicenseFriendlyNames["$LicenseItem"]  # Use global variable to get friendly name
+      $NamePrint = if ($EasyName) { $EasyName } else { $LicenseItem }
+      
+      $serviceExceptDisabled = ""
+      $FriendlyNameOfServiceExceptDisabled = ""
+      $DisabledServiceCount = 0
+      $EnabledServiceCount = 0
+      
+      $SkuInfo = Get-AzureADSubscribedSku | Where-Object { $_.SkuId -eq $LicenseItem }
+      
+      foreach ($ServicePlan in $SkuInfo.ServicePlans) {
+          $ServiceName = $ServicePlan.ServicePlanName
+          $ServiceId = $ServicePlan.ServicePlanId
+          $ServiceStatus = $_.AssignedPlans | Where-Object { $_.ServicePlanId -eq $ServicePlan.ServicePlanId } | Select-Object -ExpandProperty CapabilityStatus
+          
+          if ($ServiceStatus -eq "Disabled") {
+              $DisabledServiceCount++
+          } else {
+              $EnabledServiceCount++
+              if ($EnabledServiceCount -ne 1) {
+                  $serviceExceptDisabled += ","
+                  $FriendlyNameOfServiceExceptDisabled += ","
+              }
+              $serviceExceptDisabled += $ServiceName
+              $FriendlyNameOfServiceExceptDisabled += $ServiceArray["$ServiceName"]
+          }
+          
+          $Result = @{
+              'DisplayName'                   = $_.DisplayName
+              'UserPrinciPalName'             = $upn
+              'LicensePlan'                   = $LicenseItem
+              'FriendlyNameofLicensePlan'     = $NamePrint  # Updated to use friendly name
+              'ServiceId'                     = $ServiceId
+              'ServiceName'                   = $ServiceName
+              'ProvisioningStatus'            = $ServiceStatus
+          }
+          $Results = New-Object PSObject -Property $Result
+          $Results | Select-Object DisplayName, UserPrinciPalName, LicensePlan, FriendlyNameofLicensePlan, ServiceId, ServiceName, ProvisioningStatus | Export-Csv -Path $ExportCSV -NoTypeInformation -Append
+      }
+      
+      if ($DisabledServiceCount -eq 0) {
+          $serviceExceptDisabled = "All services"
+          $FriendlyNameOfServiceExceptDisabled = "All services"
+      }
+      
+      if ($LicensePlanWithEnabledService -ne "") {
+          $LicensePlanWithEnabledService += "; "
+          $FriendlyNameOfLicensePlanWithService += "; "
+      }
+      
+      $LicensePlanWithEnabledService += "$LicenseItem[$serviceExceptDisabled]"
+      $FriendlyNameOfLicensePlanWithService += "$NamePrint[$FriendlyNameOfServiceExceptDisabled]"
+  }
+  
+  $Output = @{
+      'Displayname'                               = $_.DisplayName
+      'UserPrincipalName'                         = $upn
+      'Country'                                   = $Country
+      'LicensePlanWithEnabledService'             = $LicensePlanWithEnabledService
+      'FriendlyNameOfLicensePlanAndEnabledService'= $FriendlyNameOfLicensePlanWithService
+  }
+  $Outputs = New-Object PSObject -Property $output
+  $Outputs | Select-Object Displayname, UserPrincipalName, Country, LicensePlanWithEnabledService, FriendlyNameOfLicensePlanAndEnabledService | Export-Csv -Path $ExportSimpleCSV -NoTypeInformation -Append
+}
+
+
+
+# Validate local admin privileges
+if (-not (Validate-LocalAdmin)) {
+  Write-Host "You do not have local admin privileges. Exiting..."
+  exit
+}
+
+# Validate and Install AzureAD module if not present
+Validate-AzureADModule
+
+# Connect to AzureAD
+Connect-AzureAD
+
+# Set output file
+$ExportCSV = ".\DetailedO365UserLicenseReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv"
+$ExportSimpleCSV = ".\SimpleO365UserLicenseReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv"
+
+# Get all subscribed SKUs in the tenant
+$subscribedSkus = Get-AzureADSubscribedSku
+
+# Create a hashtable to map SKU IDs to friendly names
+$LicenseFriendlyNames = @{}
+foreach ($sku in $subscribedSkus) {
+    $skuId = $sku.SkuId
+    $friendlyName = $sku.SkuPartNumber  # This is usually the friendly name of the license
+    $LicenseFriendlyNames["$skuId"] = $friendlyName
+}
+
+# FriendlyName list for license plan and service
+$FriendlyNameHash = Get-Content -Raw -Path .\LicenseFriendlyName.txt -ErrorAction Stop | ConvertFrom-StringData
+$ServiceArray = Get-Content -Path .\ServiceFriendlyName.txt -ErrorAction Stop
+
+# Hash table declaration
+$Result=""
+$Results=@()
+$output=""
+$outputs=@()
+
+# Get licensed user
+$LicensedUserCount=0
+
+# Check for input file/Get users from input file
+if([string]$UserNamesFile -ne "") {
+  # We have an input file, read it into memory
+  $UserNames=@()
+  $UserNames=Import-Csv -Header "DisplayName" $UserNamesFile
+  $userNames
+  foreach($item in $UserNames) {
+      Get-AzureADUser -ObjectId $item.displayname | Where-Object{$_.AssignedLicenses -ne $null} | ForEach-Object{
+          Get_UsersLicenseInfo
+          $LicensedUserCount++
+      }
+  }
+}
+# Get all licensed users
+else {
+  Get-AzureADUser -All $true | Where-Object{$_.AssignedLicenses -ne $null} | ForEach-Object{
+      Get_UsersLicenseInfo
+      $LicensedUserCount++
+  }
+}
+
+# Open output file after execution
+Write-Host "Detailed report available in: $ExportCSV"
+Write-host "Simple report available in: $ExportSimpleCSV"
+#$Prompt = New-Object -ComObject wscript.shell
+#$UserInput = $Prompt.popup("Do you want to open output files?", 0, "Open Files", 4)
+#If ($UserInput -eq 6) {
+#  Invoke-Item "$ExportCSV"
+#  Invoke-Item "$ExportSimpleCSV"
+#}
+
